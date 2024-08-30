e8c8f86010202c49d0ccac3c75ba8fe12835cdd6
zeinab-sheikhi <zeinab.sheykhie@gmail.com>
2024-08-30 22:28:47 +0200

    added revised version of my answer, and another optimal solution

--- longest_common_prefix.py
+++ longest_common_prefix.py
@@ -8,7 +8,7 @@ def longestCommonPrefix(strs):
     # i = 0
     # print(sorted(strs))
     # first_item = strs[0]
-    # while i < min(len(item) for item in strs):
+    # while i < len(min(strs)):
     #     j = 1
     #     prefix = first_item[i]
     #     while j < len(strs):
@@ -20,20 +20,44 @@ def longestCommonPrefix(strs):
     
     # return clp
 
-    # Better Solution
+    # Revised version
 
-    sorted_list = sorted(strs)
     clp = ""
-    first = sorted_list[0]
-    last = sorted_list[-1]
-    for i in range(min(len(first), len(last))):
-        if first[i] != last[i]:
-            return clp 
-        clp += first[i]
+    for i in range(len(min(strs))):
+        prefix = strs[0][i]
+        for j in range(len(strs)):
+            if prefix != strs[j][i]:
+                return clp
+        clp += prefix
     return clp
+   
+   
+    # Other Solutions
+
+    # sorted_list = sorted(strs)
+    # clp = ""
+    # first = sorted_list[0]
+    # last = sorted_list[-1]
+    # for i in range(min(len(first), len(last))):
+    #     if first[i] != last[i]:
+    #         return clp 
+    #     clp += first[i]
+    # return clp
+
+    # prefix = strs[0]
+    # for string in strs[1:]:
+    #     print(" the string is: ", string)
+    #     while string.find(prefix) != 0:
+    #         print(f"enter the while loop for this string and {string.find(prefix)}")
+    #         prefix = prefix[:-1]
+    #         print("The prefix is: ", prefix)
+    #     if not prefix:
+    #         return ""
+    # return prefix
+
 
 
 if __name__ == '__main__':
-    res = longestCommonPrefix(strs=["flower", "flow", "flight"])
-    # res = longestCommonPrefix(strs=["dog", "racecar", "car"])
+    # res = longestCommonPrefix(strs=["flower", "flow", "flight"])
+    res = longestCommonPrefix(strs=["dog", "racecar", "car"])
     print(res)
